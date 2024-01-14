fdca08d184b8e3ae43f5d80377ece9761433ca02
greathoaxer <shorelinesquare@naver.com>
2024-01-15 05:21:31 +0900

    add : alert builder without director

--- UIAlertBuilder/UIAlertBuilder/AlertBuilder.swift
+++ UIAlertBuilder/UIAlertBuilder/AlertBuilder.swift
@@ -7,6 +7,76 @@
 
 import UIKit
 
+
+
 class AlertBuilder: NSObject {
 
+    private var titleText: String? = nil
+    private var messageText: String? = nil
+    private var preferredStyle: UIAlertController.Style = .alert
+    private var proceedAction: UIAlertAction? = nil
+    private var cancelAction: UIAlertAction? = nil
+    private var actionSheetActions: [UIAlertAction]? = nil
+    
+    override init() {}
+    
+    func setTitle(_ title: String) -> AlertBuilder {
+        self.titleText = title
+        return self
+    }
+    
+    func setMessage(_ message: String) -> AlertBuilder {
+        self.messageText = message
+        return self
+    }
+    
+    func setPreferredStyle(_ style: UIAlertController.Style) -> AlertBuilder {
+        self.preferredStyle = style
+        return self
+    }
+    
+    func setProceedAction(_ title: String, _ style: UIAlertAction.Style, _ handler: ((UIAlertAction) -> Void)? = nil) -> AlertBuilder {
+        self.proceedAction = UIAlertAction(title: title, style: style, handler: handler)
+        return self
+    }
+    
+    func setCancelAction(_ title: String, _ style: UIAlertAction.Style, _ handler: ((UIAlertAction) -> Void)? = nil) -> AlertBuilder{
+        self.cancelAction = UIAlertAction(title: title, style: style, handler: handler)
+        return self
+    }
+    
+    func addAction(_ title: String, _ style: UIAlertAction.Style, _ handler: ((UIAlertAction) -> Void)? = nil) -> AlertBuilder {
+        if (actionSheetActions == nil) {
+            actionSheetActions = []
+        }
+        self.actionSheetActions?.append(UIAlertAction(title: title, style: style, handler: handler))
+        return self
+    }
+    
+    func build() -> UIAlertController {
+        let alertController = UIAlertController(title: self.titleText,
+                                                message: self.messageText,
+                                                preferredStyle: self.preferredStyle)
+        switch self.preferredStyle {
+        case .actionSheet:
+            if let actions = self.actionSheetActions {
+                for action in actions {
+                    alertController.addAction(action)
+                }
+            } else {
+                alertController.addAction(UIAlertAction(title: "취소", style: .cancel))
+            }
+        case .alert:
+            if let proceed = self.proceedAction, let cancel = self.cancelAction {
+                alertController.addAction(cancel)
+                alertController.addAction(proceed)
+            } else {
+                alertController.addAction(UIAlertAction(title: "확인", style: .default))
+            }
+        @unknown default:
+            fatalError("Cannot Build AlertBuilder")
+        }
+        
+        return alertController
+    }
 }
--- UIAlertBuilder/UIAlertBuilder/ViewController.swift
+++ UIAlertBuilder/UIAlertBuilder/ViewController.swift
@@ -11,9 +11,43 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTouchedAlertButton(_ sender: Any) {
+        self.present(
+            AlertBuilder()
+                .setTitle("이것은 타이틀")
+                .setMessage("이것은 메시지")
+                .setProceedAction("확인", .default, {_ in
+                    print("확인 누름")
+                })
+                .setCancelAction("취소", .destructive, { _ in
+                    print("취소 누름")
+                })
+                .build()
+            , animated: true)
+    }
+    
+    @IBAction func onTouchedActionButton(_ sender: Any) {
+        self.present(
+            AlertBuilder()
+                .setTitle("이것은 타이틀")
+                .setMessage("이것은 메시지")
+                .setPreferredStyle(.actionSheet)
+                .addAction("일반 버튼", .default, { _ in
+                    print("일반 버튼 누름")
+                })
+                .addAction("두번째 일반 버튼", .default) { _ in
+                    print("두번째 일반 버튼 누름")
+                }
+                .addAction("빨간 버튼", .destructive)
+                .addAction("취소", .cancel){ _ in
+                    print("액션 버튼 -> 취소 누름")
+                }
+                .build()
+            , animated: true)
+    }
 }
 
